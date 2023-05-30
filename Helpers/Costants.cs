08264510c5f283c0d3672c36d3059a437c0efa9e
DianaMatei11 <diana.matei@student.unitbv.ro>
2023-05-30 13:42:17 +0300

    Educational-Platform
    Unfinished

---
+++ Helpers/Costants.cs
@@ -0,0 +1,18 @@
+﻿using EducationalPlatform.Models.BusinessLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationalPlatform.Helpers
+{
+    public class Costants
+    {
+        static SchoolEntities dbContext = new SchoolEntities();
+        public static string connectionString = @"Data Source=LAPTOP-P7L9D7DP\SQLEXPRESS01;initial catalog=School;Integrated Security=True;MultipleActiveResultSets=True";
+
+
+    }
+}
